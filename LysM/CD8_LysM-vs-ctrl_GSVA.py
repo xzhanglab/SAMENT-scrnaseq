41afef16f1c875c06424f841bcf94c47efe3725d
fengshuoliu <116980671+fengshuoliu@users.noreply.github.com>
2024-09-11 09:06:08 -0500

    Update CD8_LysM-vs-ctrl_GSVA.py

--- LysM/CD8_LysM-vs-ctrl_GSVA.py
+++ LysM/CD8_LysM-vs-ctrl_GSVA.py
@@ -6,7 +6,7 @@ import numpy as np
 import plotly.graph_objects as go
 import os
 from plotly.io import to_image
-import textwrap  # This will help us wrap long legend text
+import textwrap
 
 # Check if kaleido is installed for image export
 try:
@@ -33,10 +33,29 @@ def load_data():
 
 df = load_data()
 
+# MSigDB table for Prefix selection
+msigdb_table = pd.DataFrame({
+    'Category': ['H: Hallmark Gene Sets', 'C1: Positional Gene Sets', 'C2:CP (Canonical Pathways)', 
+                 'C2:CGP (Chemical and Genetic Perturbations)', 'C3:MIR (MicroRNA targets)', 'C3:TFT (Transcription Factor targets)',
+                 'C4:CGN (Cancer Gene Neighborhoods)', 'C4:CM (Cancer Modules)', 'C5: GOBP (Biological Processes)',
+                 'C5: GOCC (Cellular Components)', 'C5: GOMF (Molecular Functions)', 'C6: Oncogenic Signatures',
+                 'C7: Immunologic Signatures', 'C8: Cell Type Signatures'],
+    'Prefix': ['HALLMARK', 'CHR', 'KEGG, REACTOME, BIOCARTA', 'CGP', 'MIR', 'TFT',
+               'CGN', 'CM', 'GOBP', 'GOCC', 'GOMF', 'C6, ONCOGENIC', 'C7', 'C8'],
+    'Example': ['HALLMARK_APOPTOSIS', 'CHR1Q22', 'KEGG_APOPTOSIS, REACTOME_CELL_CYCLE', 'CGP_CANCER_DRUGS', 'MIR_21', 'TFT_STAT3',
+                'CGN_P53', 'CM_APOPTOSIS', 'GOBP_APOPTOSIS', 'GOCC_NUCLEUS', 'GOMF_RECEPTOR_ACTIVITY', 'C6_MYC_TARGETS',
+                'C7_T_CELL_RECEPTOR_PATHWAY', 'C8_T_CELL_SIGNATURE']
+})
+
 # Function to categorize pathways
-def get_category(row, keywords=[], logic='AND'):
+def get_category(row, keywords=[], exclude_keywords=[], logic='AND'):
     pathway_name = row.name.replace('_', ' ').upper()
     keywords = [kw.upper().strip() for kw in keywords if kw.strip() != '']
+    exclude_keywords = [kw.upper().strip() for kw in exclude_keywords if kw.strip() != '']
+    
+    # Exclude pathways that match any excluded keywords
+    if any(keyword in pathway_name for keyword in exclude_keywords):
+        return 'non-significant'
     
     if logic == 'AND':
         if all(keyword in pathway_name for keyword in keywords):
@@ -57,8 +76,8 @@ def wrap_text(text, width=30):
     return '<br>'.join(textwrap.wrap(text, width=width))
 
 # Function to update the plot
-def update_plot(keywords=[], logic='AND', width='100%', height=800, interactive=True):
-    df['category'] = df.apply(get_category, axis=1, keywords=keywords, logic=logic)
+def update_plot(keywords=[], exclude_keywords=[], logic='AND', width='100%', height=800, interactive=True):
+    df['category'] = df.apply(get_category, axis=1, keywords=keywords, exclude_keywords=exclude_keywords, logic=logic)
     palette = {'keyword_match': '#32CD32', 'upregulated': '#FF6347', 'downregulated': '#1E90FF', 'non-significant': '#A9A9A9'}
     fig = go.Figure()
     
@@ -98,7 +117,39 @@ def update_plot(keywords=[], logic='AND', width='100%', height=800, interactive=
                                      text=f"<b style='color:black;'>{i+1}</b>",  # Bold and black color for numbers
                                      hoverinfo='text', name=wrap_text(f"{', '.join(keywords)}") if showlegend else None, showlegend=showlegend))
 
-    # Set layout
+    # Add vertical dashed lines at x = -0.2 and x = 0.2
+    fig.update_layout(
+        shapes=[
+            dict(
+                type="line",
+                x0=-0.2,
+                y0=0,
+                x1=-0.2,
+                y1=df['-log10(adj.P.Val)'].max(),
+                line=dict(
+                    color="grey",
+                    width=2,
+                    dash="dash",
+                ),
+                layer='above'  # Ensure the line is drawn above the data points
+            ),
+            dict(
+                type="line",
+                x0=0.2,
+                y0=0,
+                x1=0.2,
+                y1=df['-log10(adj.P.Val)'].max(),
+                line=dict(
+                    color="grey",
+                    width=2,
+                    dash="dash",
+                ),
+                layer='above'
+            )
+        ]
+    )
+
+    # Set layout with wrapped legend text
     fig.update_layout(
         paper_bgcolor='rgba(0,0,0,0)',
         plot_bgcolor='rgba(255,255,255,1)',
@@ -111,14 +162,9 @@ def update_plot(keywords=[], logic='AND', width='100%', height=800, interactive=
         legend_title_text='Pathway Categories',
         autosize=True,
         legend=dict(
-            orientation="v",  # Vertical legend
-            font=dict(size=10),
-            x=1.05,  # Move the legend slightly outside the plot
-            y=1,
-            traceorder='normal',
-            bgcolor='rgba(255, 255, 255, 0.5)',  # Set background with opacity
-            bordercolor='Black',
-            borderwidth=1
+            x=1, y=1, 
+            bgcolor='rgba(255,255,255,0)',  # Transparent background for legend
+            bordercolor='rgba(255,255,255,0)',  # Transparent border
         )
     )
     
@@ -130,13 +176,25 @@ if df is not None:
     num_keywords = st.sidebar.number_input('Number of Keywords', min_value=1, max_value=10, value=2)
     keywords = [st.sidebar.text_input(f'Keyword {i+1}') for i in range(num_keywords)]
     logic = st.sidebar.selectbox('Logic', ['AND', 'OR'])
+    exclude_keywords = st.sidebar.text_area('Exclude Keywords (comma-separated)').split(',')
     keywords = [kw for kw in keywords if kw.strip() != '']
-    fig_width = st.sidebar.slider('Figure Width', min_value=400, max_value=1600, value=1200, step=50)
-    fig_height = st.sidebar.slider('Figure Height', min_value=400, max_value=1200, value=1000, step=50)
+    
+    fig_width = st.sidebar.slider('Figure Width', min_value=400, max_value=1600, value=1000, step=50)
+    fig_height = st.sidebar.slider('Figure Height', min_value=400, max_value=1200, value=800, step=50)
+    
+    # Prefix selection for filtering pathways
+    st.sidebar.header('Filter Pathways by Prefix')
+    selected_prefixes = st.sidebar.multiselect('Select Prefixes', msigdb_table['Prefix'].tolist(), default=msigdb_table['Prefix'].tolist())
+    
+    # Filter the data based on selected prefixes
+    if selected_prefixes:
+        pattern = '|'.join(selected_prefixes)  # Create a pattern to match selected prefixes
+        df = df[df.index.str.contains(pattern)]
+
     interactive_keywords = st.sidebar.radio('Keyword-Matched Pathways Interactive?', ('Yes', 'No'))
     
     # Update plot based on user input
-    fig, keyword_df = update_plot(keywords, logic, width=fig_width, height=fig_height, interactive=(interactive_keywords == 'Yes'))
+    fig, keyword_df = update_plot(keywords, exclude_keywords, logic, width=fig_width, height=fig_height, interactive=(interactive_keywords == 'Yes'))
     
     # Display plot
     st.plotly_chart(fig, use_container_width=True)
@@ -146,11 +204,12 @@ if df is not None:
         st.write("### Keyword-Matched Pathways")
         st.dataframe(keyword_df[['P.Value']].reset_index().rename(columns={'index': 'Pathway'}))
 
-    st.write(f"Keywords used: {keywords}")
-    st.write(f"Logic used: {logic}")
-    st.write(f"Figure size: {fig_width} x {fig_height}")
-    st.write(f"Keyword-Matched Pathways Interactive: {interactive_keywords}")
+    # Display the MSigDB Table
+    st.write("### MSigDB Categories And Prefixes")
+    st.table(msigdb_table)
+    st.write("You can specify the category (\"Prefix\") desired for your research in search box.")
 
+    
     # Download plot as PNG or PDF
     st.sidebar.header('Download Plot')
     download_format = st.sidebar.radio('Download Format', ('PNG', 'PDF'))
